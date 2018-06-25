d21f72ad26bb24dceff5d24e3696d49b20bab7c3
DaniloSilva1 <Danilo.especialize@gmail.com>
2018-06-24 21:55:54 -0300

    Versão 1.0

---
+++ Projeto/App/Script.js
@@ -0,0 +1,17 @@
+$(document).ready(
+
+function Substitui(){
+
+$('#fotoprincipal').cycle({ 
+    fx:      'scrollLeft', 
+    speed:    3000, 
+    timeout:  2000,
+    prev: '#link_anterior',
+    next: '#link_proximo'
+});
+
+   }
+);
+
+    
+
